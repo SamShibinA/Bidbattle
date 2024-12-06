7fedbccfb1ea015217f34e26d715212f0d72b688
adhithyanms <adhithyanshanmugam@gmail.com>
2024-12-06 21:34:02 +0530

    Frontend change

---
+++ Frontend/src/Pages/Auctionseconpage/auctionsecondpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavigationBar from '../../Components/Navbar/navbar'
+import BidCard from '../../Components/AuctionSecond/bidcard'
+
+const Auctionsecondpage = () => {
+  return (
+    <div>
+      <NavigationBar />
+      <BidCard />
+    </div>
+  )
+}
+
+export default Auctionsecondpage
---
+++ Frontend/src/Pages/Paymentpage/paymentpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavigationBar from '../../Components/Navbar/navbar'
+import Payment from '../../Components/Payment/payment'
+
+const Paymentpage = () => {
+  return (
+    <div>
+      <NavigationBar />
+      <Payment />
+    </div>
+  )
+}
+
+export default Paymentpage
